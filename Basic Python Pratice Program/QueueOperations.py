79a6a94e074ceac2021b89d495fe4e2ada092e8d
yunocrown <jatin2002sep@gmail.com>
2022-10-01 15:56:58 +0530

    Pushing queueOperation

---
+++ Basic Python Pratice Program/QueueOperations.py
@@ -0,0 +1,10 @@
+#[yunocrown]( https://github.com/yunocrown )
+que = []
+que.append('Jatin')
+que.append('Rohan')
+que.append('Neeraj')
+
+#This will print the queue
+print(que)
+#This is delete the 2nd element of the queue i.e Rohan cause rohan is at 2nd index
+print(que.pop(1))
